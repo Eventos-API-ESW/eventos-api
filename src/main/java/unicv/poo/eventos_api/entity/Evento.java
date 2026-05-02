d67d2d3d4fde8b0b027aec1619b4801963c82297
João Pedro Vieira dos Santos Romano <jpvs06x0805@gmail.com>
2026-05-02 18:26:41 -0300

    fix(evento): correção de redundancia nas anotações

--- src/main/java/unicv/poo/eventos_api/entity/Evento.java
+++ src/main/java/unicv/poo/eventos_api/entity/Evento.java
@@ -1,10 +1,6 @@
 package unicv.poo.eventos_api.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,28 +21,21 @@ public class Evento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank
     @Column(nullable = false)
     private String nome;
 
-    @NotBlank
     @Column(columnDefinition = "TEXT", nullable = false)
     private String descricao;
 
-    @NotNull
-    @FutureOrPresent
     @Column(name = "data_evento", nullable = false)
     private LocalDate dataEvento;
 
-    @NotNull
     @Column(nullable = false)
     private LocalTime horario;
 
-    @Positive
     @Column(nullable = false)
     private Integer capacidade;
 
-    @NotNull
     @Column(name = "local_id", nullable = false)
     private Long localId;
 }
